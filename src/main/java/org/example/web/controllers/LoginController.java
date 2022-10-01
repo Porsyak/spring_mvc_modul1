e117691abf70dbf54d934e288374715a1e4437c4
Ivan Porsyak <2663634@gmail.com>
2022-10-01 18:39:39 +0300

    add files 404.HTML and web.xml with return line add class ErrorController

--- src/main/java/org/example/web/controllers/LoginController.java
+++ src/main/java/org/example/web/controllers/LoginController.java
@@ -9,8 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @Log4j2
 @Controller
