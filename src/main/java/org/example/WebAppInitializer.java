4a52bb2661c4a6753c0caabd6bae2fc27d7d3a7d
Ivan Porsyak <2663634@gmail.com>
2022-10-05 18:26:52 +0300

    add SpringSecurity in prodject and delete xml configure

--- src/main/java/org/example/WebAppInitializer.java
+++ src/main/java/org/example/WebAppInitializer.java
@@ -1,11 +1,11 @@
 package org.example;
 
 import lombok.extern.log4j.Log4j2;
+import org.example.app.config.AppContextConfig;
 import org.example.web.config.WebContextConfig;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
@@ -18,8 +18,10 @@ public class WebAppInitializer implements WebApplicationInitializer {
     public void onStartup(javax.servlet.ServletContext servletContext)
             throws ServletException {
         log.info("LOGGER-----loading app-config.xml-----LOGGER");
-        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-        appContext.setConfigLocation("classpath:app-config.xml");
+//        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+//        appContext.setConfigLocation("classpath:app-config.xml");
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(AppContextConfig.class);
         servletContext.addListener(new ContextLoaderListener(appContext));
 
         log.info("LOGGER-----loading web-config.xml-----LOGGER");
--- src/main/java/org/example/web/controllers/ErrorController.java
+++ src/main/java/org/example/web/controllers/ErrorController.java
@@ -1,13 +1,9 @@
 package org.example.web.controllers;
 
-import org.example.app.exception.BookShelfLoginException;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@ControllerAdvice
+@Controller
 public class ErrorController {
 
     @GetMapping("/404")
@@ -15,9 +11,4 @@ public class ErrorController {
         return "errors/404";
     }
 
-    @ExceptionHandler(BookShelfLoginException.class)
-    public String handleError(Model model, BookShelfLoginException exception){
-        model.addAttribute("errorMassage", exception.getMassage());
-        return "errors/404";
-    }
 }
